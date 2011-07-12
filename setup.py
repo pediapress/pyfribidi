31f02e50c4900756de73b6b23697ceb4c9150669
Ralf Schmitt <ralf@systemexit.de>
2011-07-12 23:19:00 +0200

    configure fribidi library and use distutils to compile the c sources

--- setup.py
+++ setup.py
@@ -2,23 +2,75 @@
 
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.command import build_ext
+
 import os
 
+USE_SYSTEM_LIB = False
+
+class my_build_ext(build_ext.build_ext):
+    def build_extension(self, ext):
+        configure = os.path.abspath("fribidi-src/configure")
+        bdir = os.path.join(self.build_temp, "fribidi")
+        if not os.path.isdir(bdir):
+            os.makedirs(bdir)
+        cwd = os.getcwd()
+        os.chdir(bdir)
+        try:
+            if not os.path.exists("./config.status"):
+                os.system("sh %s --with-glib=no" % configure)
+        finally:
+            os.chdir(cwd)
+
+        self.include_dirs[:0] = [bdir, "%s/lib" % bdir]
+        self.compiler.set_include_dirs(self.include_dirs)
+
+        return build_ext.build_ext.build_extension(self, ext)
+
+
 def _getpkgconfigvalue(value, package="fribidi"):
-	""" get a value from pkg-config for package (default: fribidi)
-	    param value: long-option to pkg-config
-	"""
-	f = os.popen("pkg-config --%s %s" % (value, package))
-	x = f.readline().strip()
-	f.close()
+    """ get a value from pkg-config for package (default: fribidi)
+    param value: long-option to pkg-config
+    """
+    f = os.popen("pkg-config --%s %s" % (value, package))
+    x = f.readline().strip()
+    f.close()
 
-	# generators: 2.4+ only :(
-	#return list(y[2:] for y in x.split(" "))
+    l = []
+    for y in x.split(" "):
+        l.append(y[2:])
+    return l
 
-	l = []
-	for y in x.split(" "):
-		l.append(y[2:])
-	return l
+if USE_SYSTEM_LIB:
+    lib_sources = []
+    include_dirs = _getpkgconfigvalue("cflags-only-I")
+    libraries = _getpkgconfigvalue("libs-only-l"),
+    define_macros = []
+    my_build_ext = build_ext.build_ext
+else:
+        lib_sources = """
+fribidi-src/lib/fribidi.c
+fribidi-src/lib/fribidi-arabic.c
+fribidi-src/lib/fribidi-bidi.c
+fribidi-src/lib/fribidi-bidi-types.c
+fribidi-src/lib/fribidi-deprecated.c
+fribidi-src/lib/fribidi-joining.c
+fribidi-src/lib/fribidi-joining-types.c
+fribidi-src/lib/fribidi-mem.c
+fribidi-src/lib/fribidi-mirroring.c
+fribidi-src/lib/fribidi-run.c
+fribidi-src/lib/fribidi-shape.c
+fribidi-src/charset/fribidi-char-sets-cp1256.c
+fribidi-src/charset/fribidi-char-sets-iso8859-8.c
+fribidi-src/charset/fribidi-char-sets-cap-rtl.c
+fribidi-src/charset/fribidi-char-sets-utf8.c
+fribidi-src/charset/fribidi-char-sets.c
+fribidi-src/charset/fribidi-char-sets-cp1255.c
+fribidi-src/charset/fribidi-char-sets-iso8859-6.c
+""".split()
+        libraries = []
+        include_dirs = ["fribidi-src", "fribidi-src/lib", "fribidi-src/charset"]
+        define_macros = [("HAVE_CONFIG_H", 1)]
 
 setup (name = "pyfribidi",
         version = "0.10.0",
@@ -27,11 +79,13 @@ setup (name = "pyfribidi",
         author_email = "kzamir@walla.co.il",
         url = "http://hspell-gui.sourceforge.net/pyfribidi.html",
         license = "GPL",
+       cmdclass={'build_ext': my_build_ext},
 
         ext_modules = [ Extension(
                 name = 'pyfribidi',
-                sources = ['pyfribidi.c'],
-                libraries = _getpkgconfigvalue("libs-only-l"),
-                include_dirs = _getpkgconfigvalue("cflags-only-I")
+                sources = ['pyfribidi.c'] + lib_sources,
+                define_macros=define_macros,
+                libraries=libraries,
+                include_dirs=include_dirs,
         )]
 )
